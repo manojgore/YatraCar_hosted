1c61ab5cd3e057b56ba39d55f556d51c838fad8f
MANOJ GORE <manojgore.mg@gmail.com>
2023-12-22 20:25:56 +0530

    d

--- app/Http/Controllers/Admin/PageTermController.php
+++ app/Http/Controllers/Admin/PageTermController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
-use App\Models\PageTermItem;
+use App\Models\PageTermItem; 
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use DB;
