886b9d1d0ded9070555660b6c3b3f6422ad27e14
MANOJ GORE <manojgore.mg@gmail.com>
2024-01-16 10:45:30 +0530

    ss

--- resources/views/front/customer_listing_view_detail.blade.php
+++ resources/views/front/customer_listing_view_detail.blade.php
@@ -22,8 +22,6 @@
                     </div>
                 </div>
                 <div class="col-md-9">
-
-
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="form-group">
