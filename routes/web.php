481f9c3ea70b20838c9d7bdeffa76e4d43af3093
MANOJ GORE <manojgore.mg@gmail.com>
2024-01-16 10:41:22 +0530

    s

--- routes/web.php
+++ routes/web.php
@@ -60,10 +60,10 @@ Route::group(['middleware' => ['XSS']], function () {
 /* --------------------------------------- */
 /* Front End */
 /* --------------------------------------- */
-// Route::get('/', [HomeController::class,'index']);
+Route::get('/', [HomeController::class,'index']);
 
-// Route::post('currency', [CurrencyControllerForFront::class,'index'])
-//     ->name('front_currency');
+Route::post('currency', [CurrencyControllerForFront::class,'index'])
+     ->name('front_currency');
 
 // Route::get('about', [AboutController::class,'index'])
 //     ->name('front_about');
