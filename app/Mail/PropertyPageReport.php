4c76b005cdf0b345ac14ae326c1009f537962efa
MANOJ GORE <manojgore.mg@gmail.com>
2024-01-26 10:14:01 +0530

    jh

--- app/Mail/PropertyPageReport.php
+++ app/Mail/PropertyPageReport.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class ListingPageReport extends Mailable
+class ListingPageReport extends Mailable 
 {
     use Queueable, SerializesModels;
 
