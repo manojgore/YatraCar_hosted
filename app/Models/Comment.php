73e6737895434f038e8ef57b5a360125a0ba0f00
MANOJ GORE <manojgore.mg@gmail.com>
2024-01-27 11:50:53 +0530

    fgd

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable = [
+    protected $fillable = [ 
         'blog_id',
         'person_name',
         'person_email',
