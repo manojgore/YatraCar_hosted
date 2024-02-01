a8eee85c4f52f8d71aae4dd8adccbc4a8d0fa672
MANOJ GORE <manojgore.mg@gmail.com>
2024-02-01 18:24:54 +0530

    hgh

--- resources/views/front/customer_listing_view_detail.blade.php
+++ resources/views/front/customer_listing_view_detail.blade.php
@@ -16,7 +16,7 @@
     <div class="page-content">
         <div class="container">
             <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-3"> 
                     <div class="user-sidebar">
                         @include('front.customer_sidebar')
                     </div>
